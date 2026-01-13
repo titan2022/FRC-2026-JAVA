005a27a6d76a6c7423230600fe1cccc2cb3e7d8a
ethanc8 <echaroenpitaks@gmail.com>
2026-01-13 09:53:56 -0600

    Import to 2026

--- src/main/java/frc/robot/drive/ctre/TunerConstants.java
+++ src/main/java/frc/robot/drive/ctre/TunerConstants.java
@@ -12,7 +12,6 @@ import com.ctre.phoenix6.swerve.SwerveModuleConstants.*;
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
-import edu.wpi.first.units.Unit;
 import edu.wpi.first.units.measure.*;
 import frc.robot.drive.ctre.CTRESwerveDrivetrain;
 
