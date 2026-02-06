e052f057c9f250db0b50366021b33e011fc41cc7
Cinna Davis <cinnajdavis@gmail.com>
2026-02-05 20:04:39 -0600

    Split up Intake and Pinion so Pinion can be used with elevator

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -11,27 +11,12 @@ public class Intake extends SubsystemBase {
   // Both of these need to be replaced with proper voltage values
 
   private static final double INTAKE_VOLTAGE = 1.0;
-  private static final double PINION_VOLTAGE = 1.0;
 
   // Use better ID values?
-  private static final TalonFX pinionMotor = new TalonFX(60, "rio");
   private static final TalonFX intakeMotor = new TalonFX(70, "rio");
 
   public Intake() {
-    pinionMotor.setNeutralMode(NeutralModeValue.Brake);
     intakeMotor.setNeutralMode(NeutralModeValue.Brake);
-  }
-
-  public void extendPinion() {
-    pinionMotor.setVoltage(PINION_VOLTAGE);
-  }
-
-  public void retractPinion() {
-    pinionMotor.setVoltage(-PINION_VOLTAGE);
-  }
-
-  public void stopPinion() {
-    pinionMotor.stopMotor();
   }
 
   public void forwardIntake() {
@@ -90,58 +75,6 @@ public class Intake extends SubsystemBase {
     public void end(boolean interrupted) {
       intake.stopIntake();
     }
-  }
-
-  
-
-  // Command to extend pinion
-  public class extendPinionCommand extends Command {
-    private final Intake intake;
-
-    public extendPinionCommand(Intake intake) {
-      this.intake = intake;
-      addRequirements(intake);
-    }
-
-    @Override
-    public void initialize() {
-      intake.extendPinion();
-    }
-
-    @Override
-    public void end(boolean interrupted) {
-      intake.stopPinion();
-    }
-
-  }
-
-  public Command extendPinionCommand() {
-    return new extendPinionCommand(this);
-  }
-
-  // Command to retract pinion
-  public class retractPinionCommand extends Command {
-    private final Intake intake;
-
-    public retractPinionCommand(Intake intake) {
-      this.intake = intake;
-      addRequirements(intake);
-    }
-
-    @Override
-    public void initialize() {
-      intake.retractPinion();
-    }
-
-    @Override
-    public void end(boolean interrupted) {
-      intake.stopPinion();
-    }
-
-  }
-
-  public Command retractPinionCommand() {
-    return new retractPinionCommand(this);
   }
 
 }
