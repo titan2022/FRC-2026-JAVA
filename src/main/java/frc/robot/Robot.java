706d98f76983ac87b316d780fff54ce3e4e9c0a9
titan2022-multiuser-system <titan2022.multiuser.system@gmail.com>
2026-03-23 17:40:53 -0500

    Removed manual addition of field multiplier to fix bug where rotation was applied twice.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -200,7 +200,8 @@ public class Robot extends TimedRobot {
 
 	@Override
 	public void autonomousInit() {
-		drivingCommand.resetAlliance();
+		// No longer called, see function for explanation
+		// drivingCommand.resetAlliance();
 
 		m_autonomousCommand = autoChooser.getSelected();
 
@@ -215,7 +216,8 @@ public class Robot extends TimedRobot {
 
 	@Override
 	public void teleopInit() {
-		drivingCommand.resetAlliance();
+		// No longer called, see function for explanation
+		// drivingCommand.resetAlliance();
 
 		if (m_autonomousCommand != null) {
 			m_autonomousCommand.cancel();
